c0a8636ee891daa887e053b6b997262e3406c3b5
Qhuy03 <huyntq2003@gmail.com>
2023-04-09 20:16:22 +0700

    'Huy'

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,46 @@
-let inputUserCMT = document.querySelector('#input-user-comment');
-let ulShow = document.querySelector("#ul-show-comment");
-let commentUserShow = document.querySelector("#input-comment");
+// let inputUserCMT = document.querySelector('#input-user-comment');
+// let ulShow = document.querySelector("#ul-show-comment");
+// let commentUserShow = document.querySelector("#input-comment");
 
-let valueInputUserCMT;
+// let valueInputUserCMT;
 
-inputUserCMT.addEventListener("change" , (event)=> {
-    const tagLi = document.querySelectorAll("#ul-show-comment li");
+// inputUserCMT.addEventListener("change" , (event)=> {
+//     const tagLi = document.querySelectorAll("#ul-show-comment li");
 
-    valueInputUserCMT = inputUserCMT.value;
+//     valueInputUserCMT = inputUserCMT.value;
 
-    let html = `<li>
-                    <div class="action-comment-user">
-                        <div class="left-action-comment-user">
-                            <img src="./assets/images/glx_trade.png" alt="">
-                        </div>
-                            <div class="right-action-comment-user">
-                                <div class="input-comment" id="input-comment">
-                                    <p>${valueInputUserCMT}</p>
-                                </div>
-                        </div>
-                     </div>
-                </li>`;
-    tagLi[tagLi.length -1].insertAdjacentHTML("afterend", html);
+//     let html = `<li>
+//                     <div class="action-comment-user">
+//                         <div class="left-action-comment-user">
+//                             <img src="./assets/images/glx_trade.png" alt="">
+//                         </div>
+//                             <div class="right-action-comment-user">
+//                                 <div class="input-comment" id="input-comment">
+//                                     <p>${valueInputUserCMT}</p>
+//                                 </div>
+//                         </div>
+//                      </div>
+//                 </li>`;
+//     tagLi[tagLi.length -1].insertAdjacentHTML("afterend", html);
 
-})
+// })
 
 
-console.log(ulShow);
+// console.log(ulShow);
 
+window.onload = function(){
+    let user_login = localStorage.getItem('user_login');
+    if(user_login){
+        document.getElementById('login').remove();
+        document.getElementById('register').remove();
+        // var htmlObj = document.getElementsByClassName('action-header')[0];
+        // htmlObj.innerHTML = htmlObj.innerHTML + `<a href="#"><i class="fa-regular fa-user">         Hello, Bro!</i></a>`;
+
+        let $profile = document.createElement('a');
+        $profile.href = "#";
+        $profile.innerHTML = `<i class="fa-regular fa-user">         Hello, Bro!</i>`;
+
+        let $bookMark = document.getElementsByClassName('bookmark-user')[0];
+        $bookMark.before($profile);
+    }
+}
