da594da55507cb216ad1f30e041c55a8c8fa6096
vietthoang24 <vietthoangg2405@gmail.com>
2023-04-09 20:36:12 +0700

    watchFilm

---
+++ assets/js/dataFilm.js
@@ -0,0 +1,290 @@
+const dataFilm = [
+    {
+        "id": "1",
+        "ten_phim" : "Lưỡng Bất Nghi",
+        "dao_dien" : "Hà Chấn Hoa",
+        "so_tap" : "1",
+        "thoi_luong" : "180",
+        "nam_san_xuat":"2019",
+        "quoc_gia":"Trung",
+        "the_loai":"Cổ trang",
+        "noi_dung":"Lưỡng Bất Nghi kể về kể về Hoàng hậu Từ Ngọc xuất thân con nhà võ, tuy nàng không giỏi cầm kỳ thi hoạ nhưng binh lược không hề thua kém đấng nam nhi nào. Từ Ngọc vào hoàng cung liền bị hoàng đế Diệp Cẩm Vân lạnh nhạt, chịu đủ loại uất ức. Nhưng bất ngờ, 1 tai nạn nhỏ xảy ra, Từ Ngọc phát hiện mình bị hoán đổi cơ thể với hoàng thượng. Sau này, Từ Ngọc giúp hoàng thượng thượng triều, xử lý triều chính, xuất binh đánh trận. Còn hoàng thượng lại giúp nàng quản lý hậu cung, xử lý phi tần, dần dần những hiểu lầm giữa hai người được xoá bỏ và bắt đầu nảy sinh tình cảm,...",
+        "link_phim" : "https://youtu.be/ugDsC3O1PYs",
+        "anh_phim":"https://4.bp.blogspot.com/-cLe6xE8esJc/WZlN8qu5kYI/AAAAAAAAFMI/cM4wI9-R0oIdzbhlKgTG9qxaQ4BadELRgCLcBGAs/s1600/Screenshot_2017-08-20-09-11-11.jpg"
+
+    },
+    {
+        "id": "2",
+        "ten_phim" : "KAMEN RIDER BLACK SUN",
+        "dao_dien" : "Shiraishi Kazuya",
+        "so_tap" : "1",
+        "thoi_luong" : "180",
+        "nam_san_xuat":"2022",
+        "quoc_gia":"Nhật Bản",
+        "the_loai":"Phiêu Lưu - Hành Động, Khoa Học - Viễn Tưởng",
+        "noi_dung":"Kamen Rider Black Sun là phiên bản kỷ niệm 50 năm của loạt phim ăn khách và cũng là phiên bản dành riêng cho người lớn (18 ). Nửa thế kỷ trôi qua kể từ khi nước Nhật thừa nhận sự tồn tại của quái vật giữa cuộc sống loài người, một kỷ nguyên hỗn loạn nổ ra. Aoi Izumi, một nhà hoạt động nhân quyền trẻ tuổi kêu gọi xóa bỏ phân biệt đối xử, gặp một người đàn ông và cùng chung tay bảo vệ đất nước.",
+        "link_phim" : "https://youtu.be/rFaUVm7dSWc",
+        "anh_phim":"https://d3nt9em9l1urz8.cloudfront.net/media/catalog/product/cache/3/image/9df78eab33525d08d6e5fb8d27136e95/4/5/4573102633309_-_s.h.figuarts_kamen_rider_black_sun_1_.jpg"
+
+    },
+    {
+        "id": "3",
+        "ten_phim" : "LÀ YÊU MÀ THÔI",
+        "dao_dien" : "Trần Sướng",
+        "so_tap" : "1",
+        "thoi_luong" : "180",
+        "nam_san_xuat":"2022",
+        "quoc_gia":"Trung Quốc",
+        "the_loai":"Tâm Lý - Tình Cảm",
+        "noi_dung":"Là Yêu Mà Thôi kể về Tinh anh công sở Lương Hữu An và vận động viên quen ngồi ghế dự bị Tống Tam Xuyên hữu duyên cùng gia nhập một CLB quần vợt. Một người là quản lý nửa mùa, một là vận động viên cầu long chuyển sang quần vợt, họ cùng đồng đội trong CLB vượt qua mọi khó khăn chung nhau phấn đấu, cố gắng tạo ra một CLB chuyên nghiệp. Trong quá trình này, Lương Hữu An và Tống Tam Xuyên dần nảy sinh tình cảm, cũng đối diện với mâu thuẫn quan niệm và kinh nghiệm. CLB không ngừng phát triển, họ lần lượt ứng phó khiêu chiến với thử thách vượt lên chính mình, tăng cường sự tin tưởng, sau cùng thu hoạch được sự trưởng thành chuyên nghiệp và tình yêu mỹ mãn.",
+        "link_phim" : "https://youtu.be/aBRs5q5Zgbw",
+        "anh_phim":"https://static2.vieon.vn/vieplay-image/thumbnail_v4/2023/03/23/8mkmn3t9_1920x1080-tinhyeumathoi1.png"
+
+    },
+    {
+        "id": "4",
+        "ten_phim" : "SÁT THỦ JOHN WICK 4",
+        "dao_dien" : "Chad Stahelski",
+        "so_tap" : "1",
+        "thoi_luong" : "160",
+        "nam_san_xuat":"2023",
+        "quoc_gia":"Âu - Mỹ",
+        "the_loai":"Phiêu Lưu - Hành Động",
+        "noi_dung":"Sát Thủ John Wick 4 là câu chuyện với cái giá phải trả ngày càng tăng, John Wick tham gia cuộc chiến chống lại High Table trên toàn cầu khi tìm kiếm những người chơi quyền lực nhất trong thế giới ngầm, từ New York qua Paris, Osaka đến cả Berlin.",
+        "link_phim" : "https://youtu.be/U8sN4H7jg_k",
+        "anh_phim":"https://ghienphim3.net/uploads/sat-thu-john-wick-phan-4-john-wick-chapter-4_banner_.jpg?v=1680153965"
+
+    },
+    {
+        "id": "5",
+        "ten_phim" : "VẠN GIỚI CHÍ TÔN",
+        "dao_dien" : "Wan Jie Zhi Zun",
+        "so_tap" : "1",
+        "thoi_luong" : "180",
+        "nam_san_xuat":"2022",
+        "quoc_gia":"Trung Quốc",
+        "the_loai":"Hoạt Hình",
+        "noi_dung":"Vạn Giới Chí Tôn nói về câu chuyện của bảy năm về trước, Sở Vân Phi gặp gia tộc tụ biến, lưu lạc đến Dược Vương Cốc ở Trung Châu. Trong bảy năm, hắn dựa vào sự cố gắng của mình, danh chấn giới tu luyện Trung Châu, sau đó trở về quê cũ báo thù, thề phải tra ra chân tướng năm đó. Trong quá trình này, hắn nhiều lần bị ám hại, trải qua cuộc điều tra khó nhằn hắn kinh ngạc phát hiện kẻ đứng sau mọi chuyện lại là đương kim đế hậu...",
+        "link_phim" : "https://youtu.be/DoLm_8LDtlo",
+        "anh_phim":"https://subnhanh.cdn1-img.net/van-gioi-chi-ton_1.jpg"
+
+    },
+    {
+        "id": "6",
+        "ten_phim" : "CON VOI CỦA NHÀ ẢO THUẬT",
+        "dao_dien" : "Wendy Rogers",
+        "so_tap" : "1",
+        "thoi_luong" : "103",
+        "nam_san_xuat":"2023",
+        "quoc_gia":"Âu - Mỹ",
+        "the_loai":"Hài Hước",
+        "noi_dung":"Con Voi Của Nhà Ảo Thuật kể về một cậu bé quyết tâm chấp nhận lời thách thức của nhà vua để thực hiện ba nhiệm vụ bất khả thi nhằm đổi lấy con voi kỳ diệu và cơ hội theo đuổi vận mệnh của mình.",
+        "link_phim" : "https://youtu.be/l6uhRm0mbY0",
+        "anh_phim":"https://afamilycdn.com/Images/Uploaded/Share/2011/04/23/042301afamilysachconvoi-2.jpg"
+
+    },
+    {
+        "id": "7",
+        "ten_phim" : "PHI HỒ NGOẠI TRUYỆN",
+        "dao_dien" : "Hà Chấn Hoa",
+        "so_tap" : "1",
+        "thoi_luong" : "180",
+        "nam_san_xuat":"2022",
+        "quoc_gia":"Trung Quốc",
+        "the_loai":" Võ Thuật - Kiếm Hiệp",
+        "noi_dung":"Phi Hồ Ngoại Truyện được chuyển thể từ bộ tiểu thuyết võ thuật nổi tiếng của nhà văn Kim Dung kể về ân oán giữa bốn gia đình Hồ, Miêu, Phạm, Điền. Trong đó Hồ Nhất Đao có ngoại hiệu là Phi thiên hồ ly và Đánh khắp thiên hạ không địch thủ Miêu Nhân Phụng vừa là đối thủ vừa là tri kỷ của nhau. Song vì âm mưu của kẻ thù mà Miêu Nhân Phụng lỡ tay giết chết Hồ Nhất Đao. Bộ phim chủ yếu xoay quanh mối quan hệ tình ái giữa con trai của Hồ Nhất Đao là Hồ Phỉ (có ngoại hiệu Tuyết Sơn Phi Hồ) và Miêu Nhược Lan, con gái của Miêu Nhân Phụng.",
+        "link_phim" : "https://youtu.be/1IzGL7d3NX4",
+        "anh_phim":"https://newsmd2fr.keeng.net/tiin/archive/imageslead/2022/09/04/90_84362b80d4e0d57d2cf082535ba202e2.jpg"
+
+    },
+    {
+        "id": "8",
+        "ten_phim" : "GỬI CẬU, TÌNH ĐẦU CỦA TÔI",
+        "dao_dien" : "Park Jin Woo, Seo Joo Wan",
+        "so_tap" : "1",
+        "thoi_luong" : "150",
+        "nam_san_xuat":"2023",
+        "quoc_gia":"Hàn Quốc",
+        "the_loai":" Tâm Lý - Tình Cảm, Gia Đình - Học Đường",
+        "noi_dung":"Gửi Cậu, Tình Đầu Của Tôi kể về một lá thư tỏ tình gửi cho “M” do một người giấu tên đăng lên diễn đàn sinh viên của trường đại học Seoyeon. Ngay sau khi đăng tải, bức thư lập tức được sinh viên trong trường bàn tán xôn xao và bắt đầu cuộc hành trình tìm kiếm nhân vật “M” trong thư.",
+        "link_phim" : "https://youtu.be/flNcbzMWkI4",
+        "anh_phim":"https://blogradio.vn/uploads/cms/source/2021/20/moitinhdaucuatoi_(1).jpg"
+
+    },
+    {
+        "id": "9",
+        "ten_phim" : "TÂN THẦN ĐIÊU ĐẠI HIỆP",
+        "dao_dien" : "Lâm Phong",
+        "so_tap" : "1",
+        "thoi_luong" : "120",
+        "nam_san_xuat":"2019",
+        "quoc_gia":"Trung Quốc",
+        "the_loai":" Cổ Trang - Thần Thoại, Võ Thuật - Kiếm Hiệp",
+        "noi_dung":"Tân Thần Điêu Đại Hiệp kể vào cuối thời Nam Tống, khi quân Mông Cổ đã lớn mạnh, tiêu diệt hầu hết châu Á, châu Âu, đang trực tiếp uy hiếp an nguy của Nam Tống. Câu chuyện xoay quanh tình yêu của hai nhân vật chính là Dương Quá và Tiểu Long Nữ giữa những cuộc chiến tang thương đẫm máu cả trên giang hồ lẫn chiến trường.",
+        "link_phim" : "https://youtu.be/wja6sUfj84o",
+        "anh_phim":"https://i.vietgiaitri.com/2018/9/15/tieu-long-nu-trong-tan-than-dieu-dai-hiep-2018-nhan-sac-than-tha-32a911.jpg"
+
+    },
+    {
+        "id": "10",
+        "ten_phim" : "AVATAR: DÒNG CHẢY CỦA NƯỚC",
+        "dao_dien" : "James Cameron",
+        "so_tap" : "1",
+        "thoi_luong" : "192",
+        "nam_san_xuat":"2022",
+        "quoc_gia":"Âu - Mỹ",
+        "the_loai":"  Khoa Học - Viễn Tưởng",
+        "noi_dung":"Thế Thân: Dòng Chảy Của Nước lấy bối cảnh 10 năm sau những sự kiện xảy ra ở phần đầu tiên. Phim kể câu chuyện về gia đình mới của Jake Sully (Sam Worthington thủ vai) cùng những rắc rối theo sau và bi kịch họ phải chịu đựng khi phe loài người xâm lược hành tinh Pandora.",
+        "link_phim" : "https://youtu.be/9lKfTWZUojw",
+        "anh_phim":"https://www.cgv.vn/media/catalog/product/cache/1/image/1800x/71252117777b696995f01934522c402d/r/s/rsz_1920x1000_1.jpg"
+
+    },
+    {
+        "id": "11",
+        "ten_phim" : "CHỊ CHỊ EM EM 2",
+        "dao_dien" : "Vũ Ngọc Đãng",
+        "so_tap" : "1",
+        "thoi_luong" : "115",
+        "nam_san_xuat":"2023",
+        "quoc_gia":"Âu - Mỹ",
+        "the_loai":" Tâm Lý - Tình Cảm",
+        "noi_dung":"Chị Chị Em Em 2 dựa trên giai thoại về hai đệ nhất mỹ nhân Sài thành Ba Trà & Tư Nhị, Minh Hằng & Ngọc Trinh mang về doanh thu hơn 120 tỷ đồng, á quân phòng vé ba tuần liên tiếp trên đường đua phim Tết 2023.",
+        "link_phim" : "https://youtu.be/2s1AsD17c20",
+        "anh_phim":"https://i.ytimg.com/vi/O1u6cEkaU0U/maxresdefault.jpg"
+
+    },
+    {
+        "id": "12",
+        "ten_phim" : " ĐÀM PHÁN",
+        "dao_dien" : "Im Soon Rye",
+        "so_tap" : "1",
+        "thoi_luong" : "108",
+        "nam_san_xuat":"2023",
+        "quoc_gia":"Hàn Quốc",
+        "the_loai":" Phiêu Lưu - Hành Động",
+        "noi_dung":"Đàm Phán kể về một nhà ngoại giao Hàn Quốc được cử đến Afghanistan khi một nhóm du khách Hàn Quốc bị Taliban bắt làm con tin. Khi mọi biện pháp đều thất bại và một con tin bị giết, anh ta buộc phải hợp tác với một đặc vụ để giải cứu những người sống sót.",
+        "link_phim" : "https://youtu.be/xCQx8Qreyos",
+        "anh_phim":"https://i.ytimg.com/vi/97hTyWINS5Y/maxresdefault.jpg"
+
+    },
+    {
+        "id": "13",
+        "ten_phim" : "VŨ ĐIỆU CUỐI CÙNG",
+        "dao_dien" : "Steven Soderbergh",
+        "so_tap" : "1",
+        "thoi_luong" : "112",
+        "nam_san_xuat":"2023",
+        "quoc_gia":"Âu - Mỹ",
+        "the_loai":" Tâm Lý - Tình Cảm",
+        "noi_dung":"Vũ Điệu Cuối Cùng kể về Mike lại lên sân khấu, sau một thương vụ phá sản, khiến anh khánh kiệt và nhận hợp đồng biểu diễn pha chế rượu ở Florida. Mike đến London cùng một người giàu có trong xã hội, người dụ dỗ anh bằng một lời đề nghị mà anh không thể từ chối.",
+        "link_phim" : "https://youtu.be/YZc_4rEslqs",
+        "anh_phim":"https://cdn.sforum.vn/sforum/wp-content/uploads/2023/02/phim-magic-mike-vu-dieu-cuoi-cung-2.jpg"
+
+    },
+    {
+        "id": "14",
+        "ten_phim" : " TÔI LÀ CHIHIRO",
+        "dao_dien" : "Imaizumi Rikiya",
+        "so_tap" : "1",
+        "thoi_luong" : "131",
+        "nam_san_xuat":"2023",
+        "quoc_gia":"Nhật Bản",
+        "the_loai":" Tâm Lý - Tình Cảm",
+        "noi_dung":"Tôi Là Chihiro kể về một người phụ nữ từng là gái mại dâm không ngại ngần bắt đầu làm việc ở quán cơm hộp tại một thị trấn nhỏ ven biển và mang lại niềm an ủi cho những tâm hồn cô đơn mà cô gặp.",
+        "link_phim" : "https://youtu.be/9GkVloV4sOg",
+        "anh_phim":"https://subnhanh.cdn1-img.net/toi-la-chihiro-call-me-chihiro-2023-vietsub_1.jpg"
+
+    },
+    {
+        "id": "15",
+        "ten_phim" : " ONE PIECE FILM RED",
+        "dao_dien" : "Gorō Taniguchi",
+        "so_tap" : "1",
+        "thoi_luong" : "115",
+        "nam_san_xuat":"2022",
+        "quoc_gia":"Nhật Bản",
+        "the_loai":" Tâm Lý - Tình Cảm",
+        "noi_dung":"One Piece Film Red kể về Bối cảnh One Piece Film Red diễn ra ở hòn đảo âm nhạc Elegia, nơi diva nổi tiếng bậc nhất thế giới tên Uta thực hiện buổi biểu diễn trực tiếp đầu tiên trước công chúng. Uta đứng trên sân khấu với một ước mơ giản dị rằng ” Âm nhạc của tôi sẽ khiến cho thế giới hạnh phúc”. Đằng sau hình ảnh cô ca sĩ sở hữu giọng hát được đánh giá là ở “Đẳng cấp hoàn toàn khác” là một thân thế vô cùng bí ẩn được che giấu. Băng hải tặc Mũ Rơm và các fan khác của Uta từ nhiều thế lực khác nhau như hải tặc lẫn hải quân đều đã cùng tề tựu về buổi biểu diễn này. Biến cố bắt đầu ngay khi sự thật kinh hoàng được tiết lộ rằng Uta chính là “con gái của Shanks”. Luffy và Uta lần đầu tiên hội ngộ sau lần gặp gỡ vào 12 năm trước tại Làng Foosha.",
+        "link_phim" : "https://youtu.be/slg_WadsGSs",
+        "anh_phim":"https://gamek.mediacdn.vn/133514250583805952/2022/4/4/fc2-164904738916349427555.jpg"
+
+    },
+    {
+        "id": "16",
+        "ten_phim" : "HUYỀN THOẠI PELÉ",
+        "dao_dien" : "Jeff Zimbalist, Michael Zimbalist",
+        "so_tap" : "1",
+        "thoi_luong" : "107",
+        "nam_san_xuat":"2016",
+        "quoc_gia":"Âu - Mỹ",
+        "the_loai":" Tài Liệu - Khám Phá, Thể Thao",
+        "noi_dung":"Huyền Thoại Pelé kể về cuộc đời huyền thoại bóng đá Pele thưở thiếu thời ở khu Sao Paulo cho đến khi dẫn dắt đội tuyển Brazil cho đến chiến thắng World Cup đầu tiên năm 17 tuổi. Đây sẽ là một bộ phim truyền cảm hứng cho tất cả những ai đã, đang và sẽ theo đuổi ước mơ trở thành một cầu thủ. Với Pelé ước mơ thôi là chưa đủ, sẽ phải nỗ lực rất nhiều để một ngày chạm tay vào chiếc cup danh giá nhất hành tinh. Bộ phim là những sự kiện nổi bất nhất trong suốt sự nghiệp của cầu thủ huyền thoại Pele. Nó đem tới những xúc cảm chân thực, mà ngôn ngữ không thể diễn tả hết.",
+        "link_phim" : "https://youtu.be/ePvma-deb4w",
+        "anh_phim":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9zSadxGzPiewj6NxUbPSId43EJJrfqJzYrWnZnQ2VvrBlhaRJ4ftM-ANVsIQzWA_56AY&usqp=CAU"
+
+    },
+    {
+        "id": "17",
+        "ten_phim" : "BLACK ADAM",
+        "dao_dien" : "Jaume Collet-Serra",
+        "so_tap" : "1",
+        "thoi_luong" : "125",
+        "nam_san_xuat":"2022",
+        "quoc_gia":"Âu - Mỹ",
+        "the_loai":"Phiêu Lưu - Hành Động, Khoa Học - Viễn Tưởng",
+        "noi_dung":"Black Adam là câu chuyện gần 5.000 năm sau khi anh ta được ban cho sức mạnh toàn năng của các vị thần Ai Cập - và bị cầm tù nhanh chóng - Black Adam được giải thoát khỏi ngôi mộ trần gian của mình, sẵn sàng giải phóng hình thức công lý độc nhất của anh ta trên thế giới hiện đại.",
+        "link_phim" : "https://youtu.be/foJvZg5K4j8",
+        "anh_phim":"https://2sao.vietnamnetjsc.vn/images/2022/10/25/11/15/black-adam-1.jpg"
+
+    },
+    {
+        "id": "18",
+        "ten_phim" : "DORAEMON: NOBITA VÀ CUỘC CHIẾN VŨ TRỤ TÍ HON",
+        "dao_dien" : "Yamaguchi Susumu",
+        "so_tap" : "1",
+        "thoi_luong" : "109",
+        "nam_san_xuat":"2022",
+        "quoc_gia": "Nhật Bản",
+        "the_loai":"Hoạt Hình",
+        "noi_dung":"Doraemon: Nobita Và Cuộc Chiến Vũ Trụ Tí Hon kể về Nobita tình cờ gặp được người ngoài hành tinh tí hon Papi, vốn là Tổng thống của hành tinh Pirika, chạy trốn tới Trái Đất để thoát khỏi những kẻ nổi loạn nơi quê nhà. Doraemon, Nobita và hội bạn thân dùng bảo bối đèn pin thu nhỏ biến đổi theo kích cỡ giống Papi để chơi cùng cậu bé. Thế nhưng, một tàu chiến không gian tấn công cả nhóm. Cảm thấy có trách nhiệm vì liên lụy mọi người, Papi quyết định một mình đương đầu với quân phiến loạn tàn ác. Doraemon và các bạn lên đường đến hành tinh Pirika, sát cánh bên người bạn của mình.",
+        "link_phim" : "https://youtu.be/R5ayIDQHOK4",
+        "anh_phim":"https://upload.wikimedia.org/wikipedia/vi/0/0e/Little_Star_Wars_2021.jpg"
+
+    },
+    {
+        "id": "19",
+        "ten_phim" : "NGÔI ĐỀN KỲ QUÁI 3",
+        "dao_dien" : "Mike Phontharis Chotkijsadarsopon",
+        "so_tap" : "1",
+        "thoi_luong" : "115",
+        "nam_san_xuat":"2022",
+        "quoc_gia": "Thái Lan",
+        "the_loai":"Hài Hước, Kinh Dị - Ma",
+        "noi_dung":"Ngôi Đền Kỳ Quái 3 tiếp tục là câu chuyện kể về hội tạo nghiệp tụ tập nhau đến dự lễ xuất gia của Aod nhưng phải hoãn lại do lời nguyền từ chiếc lắc chân xuất hiện. Min Jun, Balloon, First phải chạy đua với thời gian để tìm ra cách giải lời nguyền trước khi quỷ dữ đến lấy mạng Aod.",
+        "link_phim" : "https://youtu.be/uUt2gN03aqc",
+        "anh_phim":"https://rapchieuphim.com/photos/3/phim-ngoi-den-ky-quai/phim-ngoi-den-ky-quai.jpg"
+
+    },
+    {
+        "id": "20",
+        "ten_phim" : "Taxi Driver",
+        "dao_dien" : "Park Joon Woo",
+        "so_tap" : "1",
+        "thoi_luong" : "65",
+        "nam_san_xuat":"2021",
+        "quoc_gia": "Hàn Quốc",
+        "the_loai":"Phiêu Lưu - Hành Động",
+        "noi_dung":"kể về cuộc đời Kim Do Ki thay đổi từ sau khi mẹ anh bị sát hại bởi một kẻ giết người hàng loạt. Anh trở thành tài xế taxi hạng sang cho hãng taxi Rainbow. Đây không chỉ là một công ty taxi thông thường, mà nó cung cấp dịch vụ trả thù thay cho những người yếu thế theo yêu cầu của họ. Công việc này của hãng dĩ nhiên sẽ bị cảnh sát và bên công tố để ý điều tra. Giữa những người tin rằng sự trả thù là hiện thân của công lý và những người theo đuổi những kẻ ngoài vòng pháp luật, Do Ki sẽ tìm kiếm ý nghĩa thực sự của công lý.",
+        "link_phim" : "https://youtu.be/EG-6qAB2OQA",
+        "anh_phim":"https://image.mpapis.xyz/poster/an-danh-phan-1-11002.jpg"
+
+    }
+
+
+   
+]
+
+const filmJSON = JSON.stringify(dataFilm);
+console.log(filmJSON);
+localStorage.setItem('film',filmJSON);
+console.log(filmJSON);
